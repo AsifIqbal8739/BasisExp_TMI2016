a6b48407094b7e67b947ef99efc82cb3143e10d9
AsifIqbal8739 <Asif.Iqbal.8739@gmail.com>
2016-11-21 15:21:30 +1100

    Completed DICT REC

--- DictRec/Main_Dict_Rec.m
+++ DictRec/Main_Dict_Rec.m
@@ -1,12 +1,15 @@
-%% Main Script for Dictionary Recovery using Double Sparsity Stuff
+%% Main Script for Dictionary Recovery using Basis Expansion
+% Author "Asif Iqbal (aiqbal1@student.unimelb.edu.au)"
+% This file creates datamatrix Y and dictionary D and passes to relevant
+% function to learn a dictionary from Y and compare it to the ground truth/
 clc; clear all; close all;
 rng('default')
 
 %% Data Stuff
 m = 20;     n = 50;    g = 20;     N = 1500;      % D(m,n), Y(m,N)
-K = 2; %[2,3,4,5];        % Signal Sparsity Level
+K = 4; %[2,3,4,5];        % Signal Sparsity Level
 H = 10;     % Dictionary Sparsity Level
-SnRdB = 20; %[5,10,20,35,50];
+SnRdB = 10; %[5,10,20,35,50];
 noIt = 11*K.^2;
 Trials = 30;
 [Count_K,Count_Ks,Count_A1,Count_A2,Count_S1] = deal(zeros(Trials,noIt)); 
@@ -36,17 +39,17 @@ aa = DictR.alpha(3);
 tic;
 %% Loops and stuff
 parfor tr = 1:Trials
-    [~,~,Y1] = gererateNoiseAddedSyntheticData(N,K,Dict,SnRdB);
+    [~,~,Y] = gererateNoiseAddedSyntheticData(N,K,Dict,SnRdB);
     disp(['K-SVD Started for Trial #',num2str(tr)]);
-         [Count_K(tr,:)] = K_SVD_DR(Y1,D_base*randn(g,n),Dict,noIt,K);
+         [Count_K(tr,:)] = K_SVD_DR(Y,D_base*randn(g,n),Dict,noIt,K);
     disp(['K-SVDs Started for Trial #',num2str(tr)]);
-         [Count_Ks(tr,:)] = Dict_Rec(DictR,Y1,'ksvds');
+         [Count_Ks(tr,:)] = Dict_Rec(DictR,Y,'ksvds');
      disp(['Algo_3 Started for Trial #',num2str(tr)]);
-         [Count_A1(tr,:)] = Dict_Rec(DictR,Y1,'algo-1');
+         [Count_A1(tr,:)] = Dict_Rec(DictR,Y,'algo-1');
      disp(['Algo_4 Started for Trial #',num2str(tr)]);
-         [Count_A2(tr,:)] = Dict_Rec(DictR,Y1,'algo-2');
+         [Count_A2(tr,:)] = Dict_Rec(DictR,Y,'algo-2');
     disp(['S1 Started for Trial #',num2str(tr)]);
-         [Count_S1(tr,:)] = S_1(Y1,D_base*randn(g,n),Dict,noIt,K,aa);
+         [Count_S1(tr,:)] = S_1(Y,D_base*randn(g,n),Dict,noIt,K,aa);
 %     Count_FIDL(tr) = FIDL(Y1,D_base*randn(g,n),Dict);
 end
 warning('on','MATLAB:nearlySingularMatrix');        % turn off warning about bad-conditioned matrices
@@ -60,15 +63,15 @@ disp(['Algo_2 Ratio: ',num2str(mean(Count_A2(:,end)))]);
 disp(['S_1 Ratio: ',num2str(mean(Count_S1(:,end)))]);
 
 %% Plots
-% plot(mean(T.Count_K),'k-.','LineWidth',2)
-% hold on
-% plot(mean(T.Count_Ks),'g--','LineWidth',2)
-% plot(mean(T.Count_A1),'r-.','LineWidth',2)
-% plot(mean(T.Count_A2),'m','LineWidth',2)
-% plot(mean(T.Count_S1),'b--','LineWidth',2)
-% legend('K-SVD','K-SVDs','A_1','A_2','S_1');
-% title(['SNR = ' num2str(SnRdB),' dB']);
-% hold off
+plot(mean(Count_K),'k-.','LineWidth',2)
+hold on
+plot(mean(Count_Ks),'g--','LineWidth',2)
+plot(mean(Count_A1),'r-.','LineWidth',2)
+plot(mean(Count_A2),'m','LineWidth',2)
+plot(mean(Count_S1),'b--','LineWidth',2)
+legend('K-SVD','K-SVDs','A_1','A_2','S_1');
+title(['SNR = ' num2str(SnRdB),' dB']);
+hold off
     
 
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BasisExp_TMI2016
 Matlab code for simulation section from "Basis Expansions Approaches for Regularized Sequential Dictionary Learning Algorithms with Enforced Sparsity for fMRI Data Analysis" paper.
+
+KSVD-box, KSVDs-box, omp and ompsbox required to run the code. Please download and add them into your matlab path. 
